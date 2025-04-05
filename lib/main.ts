ad3a9c58b2156d704a5c340180994fd257688342
Miura-KR <ovis_w6v6_grappler95@yahoo.co.jp>
2025-04-06 03:37:50 +0900

    mdボタン完成

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,18 @@
 import MarkdownIt from "markdown-it";
 import Clipboard from "clipboard";
 
+export interface MarkdownItTableCopyOptions {
+  onSuccess?: (e: ClipboardJS.Event) => void;
+  onError?: (e: ClipboardJS.Event) => void;
+  copyMd?: boolean;
+  copyCsv?: boolean;
+	containerStyle?: string;
+	containerClass?: string;
+	buttonStyle?: string;
+	buttonClass?: string;
+  element?: string;
+}
+
 const tableClassName = 'markdown-it-table-copy';
 const clipboardBtnClass = `${tableClassName}-btn`;
 const tableMdValueAttr = `${tableClassName}-value`;
@@ -12,12 +24,21 @@ const	clipboard = new Clipboard(`.${clipboardBtnClass}`, {
   },
 });
 
-interface MarkdownItTableCopyOptions {
-  onSuccess?: (e: ClipboardJS.Event) => void;
-  onError?: (e: ClipboardJS.Event) => void;
-}
+const defaultOptions: MarkdownItTableCopyOptions = {
+	containerStyle: 'display: grid; grid-template: auto;',
+	containerClass: '',
+	buttonStyle: 'justify-self: end; align-self: end; cursor: pointer;',
+	// buttonStyle: 'all: unset; position: absolute; button: -10px; right: 6px;  outline: none; hight: 30px;',
+	buttonClass: '',
+  element: '<span>md</span>'
+};
 
 export function markdownitTableCopy(md: MarkdownIt, options: MarkdownItTableCopyOptions) {
+  options = {
+    ...defaultOptions,
+    ...options,
+  };
+
   if (clipboard) {
 		if (options.onSuccess) {
 			clipboard.on("success", options.onSuccess);
@@ -27,9 +48,6 @@ export function markdownitTableCopy(md: MarkdownIt, options: MarkdownItTableCopy
 		}
 	}
 
-  // 1) Core Rule:
-  //    すべての <table> トークンについて、原文（state.src）の対​応部分を抜き出して
-  //    table_open トークンに raw-data 属性として保持する
   md.core.ruler.push('table_source_extractor', function (state) {
     const tokens = state.tokens;
 
@@ -57,8 +75,6 @@ export function markdownitTableCopy(md: MarkdownIt, options: MarkdownItTableCopy
       })
   });
 
-  // 2) Render Rule:
-  //    table_open / table_close を拡張し、テーブルを <div> で包んでボタン追加する
   // 既存の table_open, table_close のレンダリングルールを退避しておく
   const originalTableOpen = md.renderer.rules.table_open ||
     function (tokens, idx, options, env, self) {
@@ -69,17 +85,17 @@ export function markdownitTableCopy(md: MarkdownIt, options: MarkdownItTableCopy
       return self.renderToken(tokens, idx, options);
     };
 
-  md.renderer.rules.table_open = function (tokens, idx, options, env, self) {
-    return `<div style="position: relative;">
-             ${originalTableOpen(tokens, idx, options, env, self)}`;
+  md.renderer.rules.table_open = function (tokens, idx, mdIdOptions, env, self) {
+    return `<div style="${options.containerStyle}" class="${options.containerClass}">
+             ${originalTableOpen(tokens, idx, mdIdOptions, env, self)}`;
   };
 
-  md.renderer.rules.table_close = function (tokens, idx, options, env, self) {
+  md.renderer.rules.table_close = function (tokens, idx, mdItOptions, env, self) {
     const buttonHtml = `
-             <button class="${clipboardBtnClass}">
-                 ${'Copy'}
+             <button class="${clipboardBtnClass} ${options.buttonClass}" style="${options.buttonStyle}">
+                 ${options.element}
              </button>`;
-    return `${originalTableClose(tokens, idx, options, env, self)}
+    return `${originalTableClose(tokens, idx, mdItOptions, env, self)}
       ${buttonHtml}
       </div>`;
   };
