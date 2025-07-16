c22e293095ab0532ebf4a9b31ce4113f5d10aeeb
Miura-KR <ovis_w6v6_grappler95@yahoo.co.jp>
2025-07-17 00:39:31 +0900

    バージョンアップによる実行コマンド修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   build: {
     lib: {
       entry: ['./lib/index.ts'],
